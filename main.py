6c3037ce2b2ab2d0996f08d779fbffb1e5aec460
Razial <uck15@bk.ru>
2023-03-17 19:27:26 +0300

    Optimize a code

--- main.py
+++ main.py
@@ -7,13 +7,17 @@ filepaths = glob.glob('Invioces/*.xlsx')
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name='Лист1')
+
     pdf = FPDF(orientation='P', unit='mm', format='A4')
     pdf.add_page()
+
     filename = Path(filepath).stem
-    filename_list = filename.split('-')
-    inv_num = filename_list[0]
-    date = f'{filename_list[1]}.{filename_list[2]}.{filename_list[3]}'
+    inv_num, date = filename.split('-')
+
     pdf.set_font(family='Times', size=16, style='B')
-    pdf.cell(w=50, h=8, txt=f'Invioces num {inv_num}', ln=1)
+    pdf.cell(w=50, h=8, txt=f'Invoices num {inv_num}', ln=1)
+
+    pdf.set_font(family='Times', size=16, style='B')
     pdf.cell(w=50, h=8, txt=f'Date {date}', ln=2)
+
     pdf.output(f'PDFs/{filename}.pdf')
